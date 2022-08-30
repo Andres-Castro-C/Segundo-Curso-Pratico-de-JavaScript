2d116d01d58da0fb98213c8a8bb22d3b49a7fd70
Andres-Castro-C <est.william.castro1@unimilitar.edu.co>
2022-08-29 21:27:14 -0500

    Agregado selectores para interactuar entre HTML y JavaScript

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,16 @@
 alert("Bienvenido al segudno curso de JavaScript");
+const h1 = document.querySelector('h1');
+const p = document.querySelector('p');
+const escuela = document.querySelector('.escuela');
+const selectores = document.querySelector('#selectores');
+const input = document.querySelector('input');
+
+console.log(input.value);
+
+console.log({
+    h1,
+    p,
+    escuela,
+    selectores,
+    input
+})
