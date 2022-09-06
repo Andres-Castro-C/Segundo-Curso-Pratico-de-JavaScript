d05c209771458c3503a75c431230e791f37634a1
Andres-Castro-C <est.william.castro1@unimilitar.edu.co>
2022-09-06 15:24:45 -0500

    Comunicandome con HTML desde JavaScript y enviamos un fromulario usando input tyype=button

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,9 @@
-alert("Bienvenido al segudno curso de JavaScript");
+console.log("Bienvenido al segudno curso de JavaScript");
 const h1 = document.querySelector('h1');
 const p = document.querySelector('p');
 const escuela = document.querySelector('.escuela');
 const selectores = document.querySelector('#selectores');
 const input = document.querySelector('input');
-
-console.log(input.value);
 
 console.log({
     h1,
@@ -14,3 +12,43 @@ console.log({
     selectores,
     input
 })
+
+
+function suma(num1,num2) {
+    return num1 + num2;
+}
+
+function resta(nu1, nu2){
+    const resta = suma(nu1,nu2);
+    console.log('La resta es ' + resta);
+}
+
+resta(2,2);
+
+const incertarTexto = document.querySelector('#incertarTexto');
+incertarTexto.innerText = 'Incertar Texto en HTML desde JavaScript';
+incertarTexto.innerText= ''; //Eliminar el Texto
+incertarTexto.classList.add('color', 'verde'); //Agregar una clase desde JavaScript
+incertarTexto.classList.remove('verde'); //Eliminar Clase desde JavaScript
+
+const imagen = document.createElement('img'); //Crear un elemanto desde JavaScript
+imagen.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Color_circle_%28hue-sat%29.png/310px-Color_circle_%28hue-sat%29.png');
+//Se crea las propiedad de la imng con setAttibute
+console.log(imagen); //Imprimimos la iamen
+
+imgJava.append(imagen); //Ponemos la imagen en HTML para que la veoa todos 
+
+const nombre = document.querySelector('#nombre'); //Reconocer datos
+const edad = document.querySelector('#edad'); //Reconocer datos
+const formulario = document.querySelector('#formulario');
+const botonEnviar = document.querySelector('#buttonEnviar');
+
+botonEnviar.addEventListener('click', botonClick);
+//formulario.addEventListener('submit', botonClick);
+function botonClick(event){ //Se crea una funcion pata hacer el llamado del Button
+    console.log('Escuchando el Boton'); //enencir el fromulario
+    //event.preventDefault(); //Enviar el fromulario sin recargar la pagina
+    nomRes.innerText = nombre.value; //Incerta el texto guardado
+    edadRes.innerText = edad.value; //Incertar la edad gaurdada
+}
+
